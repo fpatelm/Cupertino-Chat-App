a17c90a91c72f3cb68562a73bdb4764d46c43d90
Faizal Patel <faizalmw@hotmail.com>
2021-11-19 20:55:00 +0000

    add implementation for episode 5

--- lib/screens/login/user_name.dart
+++ lib/screens/login/user_name.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cupertino_chat_app/screens/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,6 +7,26 @@ import 'package:flutter/material.dart';
 class UserName extends StatelessWidget {
   UserName({Key? key}) : super(key: key);
   var _text = TextEditingController();
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  void createUserInFirestore() {
+    users
+        .where('uid', isEqualTo: FirebaseAuth.instance.currentUser.uid)
+        .limit(1)
+        .get()
+        .then(
+      (QuerySnapshot querySnapshot) {
+        if (querySnapshot.docs.isEmpty) {
+          users.add({
+            'name': _text.text,
+            'phone': FirebaseAuth.instance.currentUser.phoneNumber,
+            'status': 'Available',
+            'uid': FirebaseAuth.instance.currentUser.uid
+          });
+        }
+      },
+    ).catchError((error) {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -30,6 +51,8 @@ class UserName extends StatelessWidget {
               onPressed: () {
                 FirebaseAuth.instance.currentUser
                     .updateProfile(displayName: _text.text);
+
+                createUserInFirestore();
 
                 Navigator.push(context,
                     CupertinoPageRoute(builder: (context) => HomePage()));
--- lib/screens/people.dart
+++ lib/screens/people.dart
@@ -1,14 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cupertino_chat_app/screens/chat_detail.dart';
+import 'package:cupertino_list_tile/cupertino_list_tile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class People extends StatelessWidget {
-  const People({Key? key}) : super(key: key);
+  People({Key? key}) : super(key: key);
+  var currentUser = FirebaseAuth.instance.currentUser.uid;
+
+  void callChatDetailScreen(BuildContext context, String name, String uid) {
+    Navigator.push(
+        context,
+        CupertinoPageRoute(
+            builder: (context) => ChatDetail(
+                  friendName: name,
+                  friendUid: uid,
+                )));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("People"),
-      ),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection("users")
+            .where('uid', isNotEqualTo: currentUser)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Something went wrong"),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Text("Loading"),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return CustomScrollView(
+              slivers: [
+                CupertinoSliverNavigationBar(
+                  largeTitle: Text("People"),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    snapshot.data!.docs.map(
+                      (DocumentSnapshot document) {
+                        Map<String, dynamic> data = document.data()!;
+                        return CupertinoListTile(
+                          onTap: () => callChatDetailScreen(
+                              context, data['name'], data['uid']),
+                          title: Text(data['name']),
+                          subtitle: Text(data['status']),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                )
+              ],
+            );
+          }
+          return Container();
+        });
   }
 }
