87e6cc640a8e821971a5715e8914ed9f178a321f
Faizal Patel <fpatelm@users.noreply.github.com>
2021-12-09 16:16:47 +0000

    add implementation for episode 6

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,90 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cupertino_chat_app/screens/chat_detail.dart';
 import 'package:cupertino_list_tile/cupertino_list_tile.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Chats extends StatefulWidget {
+class Chats extends StatelessWidget {
   const Chats({Key? key}) : super(key: key);
 
   @override
-  State<Chats> createState() => _ChatsState();
-}
-
-class _ChatsState extends State<Chats> {
-  var list = [];
-  var currentUser = FirebaseAuth.instance.currentUser.uid;
-  @override
-  void initState() {
-    super.initState();
-    var tempMsg = [];
-    FirebaseFirestore.instance
-        .collection('chats')
-        .where('users.$currentUser', isEqualTo: null)
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((DocumentSnapshot document) async {
-        await FirebaseFirestore.instance
-            .collection('/chats/${document.id}/messages')
-            .orderBy('createdOn', descending: true)
-            .limit(1)
-            .get()
-            .then((QuerySnapshot querySnapshot) async {
-          if (querySnapshot.docs.isNotEmpty) {
-            await FirebaseFirestore.instance
-                .collection('/users')
-                .where('uid',
-                    isEqualTo: querySnapshot.docs.first.data()!['uid'])
-                .limit(1)
-                .get()
-                .then((QuerySnapshot querySnapshotUser) {
-              print(querySnapshot.docs.first.data()!['msg']);
-              print(querySnapshotUser.docs.first.data()!['uid']);
-              print(querySnapshotUser.docs.first.data()!['name']);
-              tempMsg.add({
-                "msg": querySnapshot.docs.first.data()!['msg'],
-                "uid": querySnapshotUser.docs.first.data()!['uid'],
-                "name": querySnapshotUser.docs.first.data()!['name']
-              });
-            }).catchError((error) {});
-          }
-        }).catchError((error) {});
-      });
-    }).catchError((error) {});
-
-    setState(() {
-      list = tempMsg;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        CupertinoSliverNavigationBar(
-          largeTitle: Text("Chats"),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate(list.map((element) {
-            return CupertinoListTile(
-              title: Text(element['name']),
-              subtitle: Text(element['msg']),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  CupertinoPageRoute(
-                    builder: (context) => ChatDetail(
-                      friendName: element['name'],
-                      friendUid: element['uid'],
-                    ),
-                  ),
-                );
-              },
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("chats").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Something went wrong"),
             );
-          }).toList()),
-        )
-      ],
-    );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Text("Loading"),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return CustomScrollView(
+              slivers: [
+                CupertinoSliverNavigationBar(
+                  largeTitle: Text("Chats"),
+                ),
+                SliverList(
+                    delegate: SliverChildListDelegate(
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map<String, dynamic> data = document.data()!;
+                  return Container();
+                }).toList()))
+              ],
+            );
+          }
+          return Container();
+        });
   }
 }
